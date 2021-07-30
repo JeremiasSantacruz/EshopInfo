be70b2e3e81db663206ff1125b4a0328de4995e6
Jeremias C. Santacruz <jeresanta@gmail.com>
2021-07-30 09:29:09 -0300

    Eliminado carpeta .idea

---
+++ src/main/java/com/informatorio/eshop/models/Usuario.java
@@ -0,0 +1,34 @@
+package com.informatorio.eshop.models;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class Usuario {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "id", nullable = false)
+   private Long id;
+
+   @NotNull
+   private String nombre;
+
+   @NotNull
+   private String apellido;
+
+   @NotNull
+   private String dirección;
+
+   @CreationTimestamp
+   private LocalDate fechaDeAlta;
+
+}
